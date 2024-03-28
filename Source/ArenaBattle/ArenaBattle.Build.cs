cef71a2683e24c1f161225f8eedee9796edf7b9f
정호진 <dnjfs@naver.com>
2024-03-28 12:47:26 +0900

    1강 언리얼 엔진 게임 제작 기초
    - 콘텐츠 팩으로 삼인칭 템플릿 추가
    
    - GameMode, PlayerController, Character 클래스 추가
    Step2 맵의 게임 모드는 ABGameMode로 설정
    게임 모드의 DefaultPawn으로 삼인칭 템플릿의 마네킹 설정
    게임 모드의 PlayerControllerClass로 ABPlayerController 설정
    게임 시작 시 마우스 포커스가 게임으로 바뀌도록 InputMode 세팅

--- Source/ArenaBattle/ArenaBattle.Build.cs
+++ Source/ArenaBattle/ArenaBattle.Build.cs
@@ -7,8 +7,10 @@ public class ArenaBattle : ModuleRules
 	public ArenaBattle(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+
+        PublicIncludePaths.AddRange(new string[] { "ArenaBattle" });
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
