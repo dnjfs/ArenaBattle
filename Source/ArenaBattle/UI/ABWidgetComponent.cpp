e1bfd2c779c82a314863d28e9cdcba8a2b479219
정호진 <dnjfs@naver.com>
2024-04-04 10:02:40 +0900

    7강 - 캐릭터 스탯과 위젯
    - 캐릭터 스탯 컴포넌트 추가
    현재 체력을 가지고 체력이 변할 때 델리게이트 전달
    캐릭터에 부착
    
    - HPBar 구현
    UMG 사용을 위하여 build 파일에 모듈 추가
    위젯 블루프린트로 생성
    현재 위젯의 Owner 액터를 알기 위해 WidgetComponent와 UserWidget을 상속받은 클래스 각각 추가
    체력이 변하면 HPBar의 값도 변하도록 구현

---
+++ Source/ArenaBattle/UI/ABWidgetComponent.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/ABWidgetComponent.h"
+#include "ABUserWidget.h"
+
+void UABWidgetComponent::InitWidget()
+{
+	Super::InitWidget();
+
+	// 위젯 인스턴스가 생성된 직후
+	UABUserWidget* ABUserWidget = Cast<UABUserWidget>(GetWidget());
+	if (ABUserWidget)
+	{
+		ABUserWidget->SetOwningActor(GetOwner());
+	}
+}
